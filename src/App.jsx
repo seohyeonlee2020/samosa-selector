fbe1630c06c54435ddcb5601f98164c4fe4c0489
seohyeon-lee-2025 <seohyeonlee2020@gmail.com>
2024-09-25 10:43:49 +0900

    lab complete

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import './App.css';
+import { useState } from 'react';
+
+const App = () => {
+	const [count, setCount] = useState(0);
+	const [multiplier, setMultiplier] = useState(1);
+	const updateCount = () => setCount(count + multiplier);
+
+  return (
+    <div className="App">
+		<div className="header">
+			<h1>Samosa Selector</h1>
+			<h2>Count: {count}</h2>
+			<img className="samosa" src="https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe.jpg"
+			alt="Samosa"  onClick={updateCount}/>
+		</div>
+
+	<div className = "container">
+		<div className = "upgrade">
+			<h3>Double Stuffed 👯‍♀️</h3>
+			<p>Double the samosas per click</p>
+			<button>10 samosas</button>
+		</div>
+
+		<div className = "upgrade">
+			<h3>Party Pack 🎉</h3>
+			<p>5x per click</p>
+			<button>100 samosas</button>
+		</div>
+
+		<div className = "upgrade">
+			<h3>Full Feast 👩🏽‍🍳</h3>
+			<p>10x per click</p>
+			<button>1000 samosas</button>
+		</div>
+    </div>
+	</div>
+  )
+}
+
+export default App
